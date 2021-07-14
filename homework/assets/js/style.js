38c0d1056e33658af4b717026872f1b466a081d2
Eltun-Ismayilov <eltunhi@code.edu.az>
2021-07-14 16:15:00 +0400

    eve

---
+++ homework/assets/js/style.js
@@ -0,0 +1,126 @@
+
+// customConcat([1,2,3,4,5],[6,7,8,9]);//[1,2,3,4,5,6,7,8,9];
+
+// function customConcat(a,b){
+
+//     const result=[];
+//    for (let i = 0; i < a.length; i++) {
+//        result.push(a[i])
+       
+//    }
+//    for (let i = 0; i < b.length; i++) {
+//        result.push(b[i])
+       
+//    }
+//    return result;
+
+
+
+// }
+
+// console.log(customConcat([1,2,3,4,5],[6,7,8,9]))
+
+
+
+
+// customConcat(1,2,3,4,5,[6,7,8,9]);//[1,2,3,4,5,6,7,8,9];
+
+// function customConcat(arr){
+// const result=[];
+
+// for (let i = 0; i < arguments.length; i++) {
+//     result.push(arguments[i])
+    
+    
+    
+// }
+// return result;
+// }
+
+// console.log(customConcat(1,2,3,4,5,[6,7,8,9]))
+
+
+
+
+
+
+// function reverse(arr) {
+//     const result = [];
+//     for (let i = arr.length - 1; i >= 0; i--) {
+//       result.push(arr[i]);
+//     }
+//     return result;
+//   }
+
+
+//   console.log(reverse([2,3,4,56,5,8])) // reverse
+  
+
+
+
+
+
+// const numbur=[1,2,3,4,5];
+
+// const evens=numbur.filter(function(item){
+
+
+//     return item % 2 == 0;
+
+// })
+
+// console.log(evens); // filter
+
+
+
+
+
+// const numbur=[1,2,3,4,5];
+
+//  const evens=numbur.map(function(item){
+
+
+//     return item * 2 ;
+
+//  })
+
+//  console.log(evens); // map
+
+
+
+
+
+// const arr=[1,2,3,4,5];
+
+// arr.forEach(function(item,index,array){
+
+//     console.log(item,index,array)
+
+// }) // foreach
+
+
+
+
+
+// const number=[1,2,3,4,5].every(function(item){
+
+//     return  item>0;
+
+// });
+
+// console.log(number) // every
+
+
+
+
+
+// const number=[1,2,3,4,5].some(function(item){
+
+//     return  item>0;
+
+// });
+
+// console.log(number) // some
+
+
+
